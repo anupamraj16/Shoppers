9f17a41eb96e3f5b51c183b5433b687a0aaeaf25
Anupam Raj <raj.anupam16@gmail.com>
2020-07-08 16:05:43 -0600

    Initial Commit

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-'Hello Shopping'
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello from the server side!', app: 'shop' });
+});
+
+app.post('/', (req, res) => {
+  res.status(200).send('You can post to this endpoint!');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
