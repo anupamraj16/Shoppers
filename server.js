18c7d68e8dd24747c2d666059454f339288ebf1c
Anupam Raj <raj.anupam16@gmail.com>
2020-08-20 17:58:12 -0600

    Heroku Ready

--- server.js
+++ server.js
@@ -20,9 +20,11 @@ mongoose
   })
   .then(() => console.log('DB connection successful...'));
 
+const port = process.env.PORT || 5000;
+
 // Start Server
-const server = app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server running...`);
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
 });
 
 // Close Server on unhandled rejections
